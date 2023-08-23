104085b818d9b939131e3c7838a02c0f74c94d27
Danilo-Lapa11 <danwanderleylapa@gmail.com>
2023-08-23 20:20:11 -0300

    Passo 2 Done - criar o grafo com os dados

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import pandas as pd
 from geopy.distance import geodesic as gdist
 
+class Graph:
+
+	def __init__(self, vertices):
+		self.V = vertices # qtd de vertices 
+		self.graph = []
+
+	# Function to add an edge to graph
+	def addEdge(self, p1, lat1, lon1, p2, lat2, lon2):
+		
+		# # calculando a distância entre os pontos usando latitude e longitude de cada aeroporto 
+		w = gdist((lat1, lon1), (lat2, lon2)).kilometers
+
+		self.graph.append([p1, p2, round(w)])
+
+
 # Leitura da base de dados
-db = pd.read_csv('Kruskal-Algorithm/airports.csv')
+db = pd.read_csv('airports.csv')
 
-# Captação dos dados da base de dados
-for idx, row in db.iterrows():
-    print((row['AIRPORT'], row['LATITUDE'], row['LONGITUDE']))
+# Instância do grafo
+g = Graph(len(db))
 
-
-
-
-
+# # Captação dos dados da base de dados
+# for idx, row in db.iterrows():
+# 	print((row['AIRPORT'], row['LATITUDE'], row['LONGITUDE']))
 
 
+g.addEdge('Yakutat', 59.50336056, -139.6602261, 'Yuma MCAS-Yuma International', 32.65658333, -114.6059722)
+g.addEdge('Eglin Air Force Base', 30.48325, -86.5254, 'Northwest Arkansas Regional', 36.28186944, -94.30681111)
+g.addEdge('Yellowstone', 44.68839917, -111.1176375, 'Yuma MCAS-Yuma International', 32.65658333, -114.6059722)
 
 
+print(g.graph)
 
-
-
-
-
-# vou add isso dps 
-
-# # calculando a distância entre os pontos usando lat e lon de cada aeroporto 
-# distance = gdist((39.04614278, -84.6621725), (32.84711389, -96.85177222)).kilometers
 
